a2bf166325eed8a232ee523892fdda0d503c8129
Bhryno <bryanrplee@gmail.com>
2026-02-08 13:39:46 +1100

    Refactor Edge and FlowGraph classes to use integer indices for nodes. Implement flow management methods and adjacency list structure for better performance.

--- Algorithm.Utilities/Edge.cs
+++ Algorithm.Utilities/Edge.cs
@@ -2,13 +2,15 @@ namespace Algorithm.Utilities;
 
 public class Edge
 {
-    public required Node From { get; init; }
+    public required int From { get; init; }
     
-    public required Node To { get; init; }
+    public required int To { get; init; }
     
     public required int Capacity { get; init; }
     
-    public int Flow { get; init; }
+    public int Flow { get; set; }
+    
+    public int ReverseIndex { get; set; }
     
     public int ResidualCapacity => Capacity - Flow;
 }
